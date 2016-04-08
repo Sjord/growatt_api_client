1b3b84662b9c23345ff328ae3e481eb64cc62eb5
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2016-04-08 14:19:10 +0200

    Python script to get some information from the Growatt API

---
+++ README.md
@@ -0,0 +1,3 @@
+## Growatt API client
+
+This Python script logs in to server.growatt.com and retrieves data on solar panels.
---
+++ growattapi.py
@@ -0,0 +1,78 @@
+from enum import IntEnum
+import datetime
+import hashlib
+import json
+import requests
+
+
+def hash_password(password):
+    password_md5 = hashlib.md5(password.encode('utf-8')).hexdigest()
+    for i in range(0, len(password_md5), 2):
+        if password_md5[i] == '0':
+            password_md5 = password_md5[0:i] + 'c' + password_md5[i + 1:]
+    return password_md5
+
+
+class Timespan(IntEnum):
+    day = 1
+    month = 2
+
+
+class GrowattApi:
+    server_url = 'http://server.growatt.com/'
+
+    def __init__(self):
+        self.session = requests.Session()
+
+    def get_url(self, page):
+        return self.server_url + page
+
+    def login(self, username, password):
+        password_md5 = hash_password(password)
+        response = self.session.post(self.get_url('LoginAPI.do'), data={
+            'userName': username,
+            'password': password_md5
+        })
+        data = json.loads(response.content.decode('utf-8'))
+        return data['back']
+
+    def plant_list(self, user_id):
+        response = self.session.get(self.get_url('PlantListAPI.do'),
+                                    params={'userId': user_id},
+                                    allow_redirects=False)
+        if response.status_code != 200:
+            raise RuntimeError("Request failed: %s", response)
+        data = json.loads(response.content.decode('utf-8'))
+        return data['back']
+
+    def plant_detail(self, plant_id, timespan, date):
+        assert timespan in Timespan
+        if timespan == Timespan.day:
+            date_str = date.strftime('%Y-%m-%d')
+        elif timespan == Timespan.month:
+            date_str = date.strftime('%Y-%m')
+
+        response = self.session.get(self.get_url('PlantDetailAPI.do'), params={
+            'plantId': plant_id,
+            'type': timespan.value,
+            'date': date_str
+        })
+        data = json.loads(response.content.decode('utf-8'))
+        return data['back']
+
+
+if __name__ == "__main__":
+    username = ...
+    password = ...
+
+    assert hash_password("banaan") == "31d674be46e1ba6b54388a671cc9accb"
+
+    api = GrowattApi()
+    login_res = api.login(username, password)
+    user_id = login_res['userId']
+    plant_info = api.plant_list(user_id)
+    print(plant_info)
+
+    plant_id = plant_info['data'][0]['plantId']
+    plant_detail = api.plant_detail(23528, Timespan.day, datetime.date.today())
+    print(plant_detail)
