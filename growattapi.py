f0157b767c71ceafeff491d76ada730a112a3997
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2019-01-29 10:44:49 +0100

    Format code
    And get username and password from command line so that the script has valid
    Python syntax.

--- growattapi.py
+++ growattapi.py
@@ -3,16 +3,17 @@ import datetime
 import hashlib
 import json
 import requests
+import sys
 
 
 def hash_password(password):
     """
     Normal MD5, except add c if a byte of the digest is less than 10.
     """
-    password_md5 = hashlib.md5(password.encode('utf-8')).hexdigest()
+    password_md5 = hashlib.md5(password.encode("utf-8")).hexdigest()
     for i in range(0, len(password_md5), 2):
-        if password_md5[i] == '0':
-            password_md5 = password_md5[0:i] + 'c' + password_md5[i + 1:]
+        if password_md5[i] == "0":
+            password_md5 = password_md5[0:i] + "c" + password_md5[i + 1 :]
     return password_md5
 
 
@@ -22,7 +23,7 @@ class Timespan(IntEnum):
 
 
 class GrowattApi:
-    server_url = 'https://server.growatt.com/'
+    server_url = "https://server.growatt.com/"
 
     def __init__(self):
         self.session = requests.Session()
@@ -32,50 +33,51 @@ class GrowattApi:
 
     def login(self, username, password):
         password_md5 = hash_password(password)
-        response = self.session.post(self.get_url('LoginAPI.do'), data={
-            'userName': username,
-            'password': password_md5
-        })
-        data = json.loads(response.content.decode('utf-8'))
-        return data['back']
+        response = self.session.post(
+            self.get_url("LoginAPI.do"),
+            data={"userName": username, "password": password_md5},
+        )
+        data = json.loads(response.content.decode("utf-8"))
+        return data["back"]
 
     def plant_list(self, user_id):
-        response = self.session.get(self.get_url('PlantListAPI.do'),
-                                    params={'userId': user_id},
-                                    allow_redirects=False)
+        response = self.session.get(
+            self.get_url("PlantListAPI.do"),
+            params={"userId": user_id},
+            allow_redirects=False,
+        )
         if response.status_code != 200:
             raise RuntimeError("Request failed: %s", response)
-        data = json.loads(response.content.decode('utf-8'))
-        return data['back']
+        data = json.loads(response.content.decode("utf-8"))
+        return data["back"]
 
     def plant_detail(self, plant_id, timespan, date):
         assert timespan in Timespan
         if timespan == Timespan.day:
-            date_str = date.strftime('%Y-%m-%d')
+            date_str = date.strftime("%Y-%m-%d")
         elif timespan == Timespan.month:
-            date_str = date.strftime('%Y-%m')
+            date_str = date.strftime("%Y-%m")
 
-        response = self.session.get(self.get_url('PlantDetailAPI.do'), params={
-            'plantId': plant_id,
-            'type': timespan.value,
-            'date': date_str
-        })
-        data = json.loads(response.content.decode('utf-8'))
-        return data['back']
+        response = self.session.get(
+            self.get_url("PlantDetailAPI.do"),
+            params={"plantId": plant_id, "type": timespan.value, "date": date_str},
+        )
+        data = json.loads(response.content.decode("utf-8"))
+        return data["back"]
 
 
 if __name__ == "__main__":
-    username = ...
-    password = ...
+    username = sys.argv[1]
+    password = sys.argv[2]
 
     assert hash_password("banaan") == "31d674be46e1ba6b54388a671cc9accb"
 
     api = GrowattApi()
     login_res = api.login(username, password)
-    user_id = login_res['userId']
+    user_id = login_res["userId"]
     plant_info = api.plant_list(user_id)
     print(plant_info)
 
-    plant_id = plant_info['data'][0]['plantId']
+    plant_id = plant_info["data"][0]["plantId"]
     plant_detail = api.plant_detail(plant_id, Timespan.day, datetime.date.today())
     print(plant_detail)
