4e0ebf4a2cbc7305001493531a03f73e193b6bd4
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2019-02-09 11:03:01 +0100

    Add enum34 requirement when we don't have enum
    This makes it compatible with Python 2.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+requirements = ["requests"]
+try:
+    import enum
+except:
+    requirements += ["enum34"]
+
 setup(
     name="growatt",
     version="0.0.2",
@@ -7,6 +13,6 @@ setup(
     url="https://github.com/Sjord/growatt_api_client",
     author="Sjoerd Langkemper",
     packages=["growatt"],
-    install_requires=["requests"],
+    install_requires=requirements,
     license="OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
 )
