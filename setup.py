bcb480393c31d13ec646940b42d8174775a57d2b
Tim van Cann <timvancann@gmail.com>
2019-02-10 17:28:16 +0100

    Match upstream license

--- setup.py
+++ setup.py
@@ -1,23 +1,34 @@
 from setuptools import setup
 
+requirements = ["requests"]
+try:
+    import enum
+except:
+    requirements += ["enum34"]
+
 with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
     name='Growatt API',
     description='A package to query the growatt server',
-    version='0.0.1',
-    author=['Sjord', 'Tim van Cann'],
-    author_email="timvancann@gmail.com",
+    version='0.0.3',
+    author=['Sjoerd Langkemper', 'Tim van Cann'],
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/timvancann/growatt_api_client",
+    license="OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
     packages=['growatt_api'],
-    install_requires=[
-        'requests==2.21.0'
-    ],
+    install_requires=requirements,
     extras_require={
         'tests': [
             'pytest==4.1.1',
             'requests-mock==1.5.2'
         ]
     },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python",
+    ],
 )
